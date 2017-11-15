68ac8b6f2545c3f25c93db6e00d7458678bf167f
hrist0stoichev <hrist0.stoichev@mail.bg>
2017-11-15 21:27:07 +0200

    auction visualization added

--- src/main/resources/static/auctionspage.js
+++ src/main/resources/static/auctionspage.js
@@ -3,14 +3,32 @@ window.onload = function() {
         var auctions = JSON.parse(response);
         var list = document.getElementById('auctions');
         list.innerHTML = '';
+
         auctions.forEach(function (auction) {
-            var listItem = document.createElement("LI");
-            var listText = document.createTextNode(auction.name);
-            listItem.appendChild(listText);
-            list.appendChild(listItem);
+            var container = document.createElement("div");
+            container.setAttribute("class", "auction");
+            container.setAttribute("id", auction.id);
+
+            var img = document.createElement("img");
+            img.setAttribute("src", auction.image);
+            img.setAttribute("alt", "Image");
+
+            var content = document.createElement("div");
+            content.innerHTML = `Name: ${auction.name}<br />
+                                 Price: ${auction.price}<br />
+                                 Start date: ${auction.startDate}<br />
+                                 End date: ${auction.endDate}`;
+
+            container.appendChild(img);
+            container.appendChild(content);
+
+            list.appendChild(container);
         });
     });
 };
+
+// var listText = document.createTextNode(auction.name);
+// listItem.appendChild(listText);
 
 function searchCategory() {
     var categoryInput = document.getElementById('categoryInput').value;
